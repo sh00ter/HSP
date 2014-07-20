9dc10e4162aec363da15ffe7caea79073bad19d9
Bogdan Balc <bogdan.balc@gmail.com>
2014-07-20 14:55:24 +0300

    small tweaks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,9 @@ var gulp = require('gulp'),
 
 var EXPRESS_PORT = 4000;
 var EXPRESS_ROOT = __dirname;
-var LIVERELOAD_PORT = 4002;
+// don't set this port to the default livereload 35729 since we are not using
+// the browser plugin but instead we are inserting the script via connect-livereload
+var LIVERELOAD_PORT = EXPRESS_PORT + 1;
 var sources = {
   sass: 'stylesheets/sass/**/*.scss',
   css: ['stylesheets/css/**/*.css', '!stylesheets/css/**/*.min.css', '!stylesheets/css/**/*.css.map'],
@@ -68,6 +70,7 @@ gulp.task('styles:sass', function() {
 
 gulp.task('styles:opt', function() {
   return gulp.src(sources.css)
+    .pipe(plumber())
     .pipe(autoprefixer(["last 1 version", "> 1%", "ie 9", "ie 8"], { cascade: true }))
     .pipe(size({ title: 'css size' }))
     .pipe(rename({ suffix: '.min' }))
@@ -75,6 +78,29 @@ gulp.task('styles:opt', function() {
     .pipe(csso())
     .pipe(gulp.dest('stylesheets/css'))
     .pipe(size({ title: 'optimized css size' }));
+});
+
+// WIP: attempt to write a task for optimizing build speed
+// should only compile changed files in .css-cache, optimize and move them to
+// stylesheets/css
+gulp.task('styles:cache', function() {
+  gulp.src(sources.sass)
+    .pipe(size({ title: 'sass size' }))
+    .pipe(plumber())
+    .pipe(compass({
+      config_file: './config.rb',
+      css: '.css-cache',
+      sass: 'stylesheets/sass'
+    }))
+    .pipe(duration('sass files compiled'));
+  
+  return watch({ glob: '.css-cache/**/*.css' }, function(files) {
+    return files
+      .pipe(plumber())
+      .pipe(cmq())
+      .pipe(csso())
+      .pipe(gulp.dest('stylesheets/css'))
+  });
 });
 
 gulp.task('styles', function() {
