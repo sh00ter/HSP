4184ea1c59823976deef4e65311ee1c3883c69de
Paul Axente <paul.axente@3pillarglobal.com>
2014-11-13 22:25:49 +0200

    [susy]: time to shine :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,134 +1,159 @@
+// -------------------------------------------------------------------------------------------------------------------------
+// Gulp Plugins ------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+// Quick npm install command -----------------------------------------------------------------------------------------------
+// $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-uglify gulp-imagemin gulp-rename gulp-concat gulp-cache gulp-livereload gulp-plumber gulp-html-replace gulp-minify-html gulp-connect del gulp-gzip --save-dev
+// -------------------------------------------------------------------------------------------------------------------------
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    express = require('express'),
-    livereload = require('connect-livereload'),
-    reload = require('gulp-livereload'),
-    path = require('path'),
-    compass = require('gulp-compass'),
+    sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    csso = require('gulp-csso'),
-    cmq = require('gulp-combine-media-queries'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
     plumber = require('gulp-plumber'),
-    duration = require('gulp-duration'),
-    runSequence = require('run-sequence'),
-    watch = require('gulp-watch'),
-    inject = require('gulp-inject'),
-    replace = require('gulp-replace'),
-    insert = require('gulp-insert'),
-    inlineCss = require('gulp-inline-css'),
-    size = require('gulp-size'),
-    args = require('yargs').argv,
-    // when using 'gulp --theme shop' or 'gulp watch --theme shop'
-    // var theme can be used to setup the theme paths
-    // if the theme argument is not used the theme var defaults to _scaffold
-    // not used at the moment
-    theme = args.theme || '_scaffold';
+    htmlreplace = require('gulp-html-replace'),
+    minifyHTML = require('gulp-minify-html'),
+    browserify = require('gulp-browserify'),
+    connect = require('gulp-connect'),
+    del = require('del'),
+    gzip = require('gulp-gzip');
 
-var EXPRESS_PORT = 4000;
-var EXPRESS_ROOT = __dirname;
-// don't set this port to the default livereload 35729 since we are not using
-// the browser plugin but instead we are inserting the script via connect-livereload
-var LIVERELOAD_PORT = EXPRESS_PORT + 1;
-var sources = {
-  sass: 'stylesheets/sass/**/*.scss',
-  css: ['stylesheets/css/**/*.css', '!stylesheets/css/**/*.min.css', '!stylesheets/css/**/*.css.map'],
-  js: ['js/**/*.js', '!js/**/*.min.js'],
-  watch: ['**/*.html', 'stylesheets/css/style.css', 'js/**/*.js']
-};
-var destinations = {
-  html: 'dist/',
-  js: 'dist/js/',
-  css: 'dist/css/'
-};
-
-
-gulp.task('serve', function() {
-  var app = express();
-  // insert livereload script so we don't need the browser plugin
-  app.use(livereload({ port: LIVERELOAD_PORT }));
-  app.use(express.static(EXPRESS_ROOT));
-  app.listen(EXPRESS_PORT);
-});
-
-refresh = function(event) {
-  var fileName = path.relative(EXPRESS_ROOT, event.path);
-
-  gutil.log(gutil.colors.magenta(fileName), gutil.colors.cyan('changed'));
-
-  gulp.src(fileName, { read: false })
-      .pipe(reload(LIVERELOAD_PORT));
-}
-
-gulp.task('styles:sass', function() {
-  return gulp.src(sources.sass, { base: './stylesheets/sass' })
-    .pipe(size({ title: 'sass size' }))
-    // inject responsive function into sass files
-    // .pipe(insert.wrap('@include r(', ')'))
-    // .pipe(insert.append(');'))
-    // .pipe(gulp.dest('stylesheets/sass'))
-    // replace code from sass files if necessary
-    // .pipe(replace('@include r(', ''))
-    // .pipe(replace(',);', ''))
-    // .pipe(gulp.dest('stylesheets/sass'))
-    .pipe(plumber())
-    .pipe(compass({
-      config_file: './config.rb',
-      css: 'stylesheets/css',
-      sass: 'stylesheets/sass'
-    }))
-    .pipe(duration('sass files compiled'));
-});
-
-gulp.task('styles:opt', function() {
-  return gulp.src(sources.css)
-    .pipe(plumber())
-    .pipe(autoprefixer(["last 1 version", "> 1%", "ie 9", "ie 8"], { cascade: true }))
-    .pipe(size({ title: 'css size' }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(cmq())
-    .pipe(csso())
-    .pipe(gulp.dest('stylesheets/css'))
-    .pipe(size({ title: 'optimized css size' }));
-});
-
-// WIP: attempt to write a task for optimizing build speed
-// should only compile changed files in .css-cache, optimize and move them to
-// stylesheets/css
-gulp.task('styles:cache', function() {
-  gulp.src(sources.sass)
-    .pipe(size({ title: 'sass size' }))
-    .pipe(plumber())
-    .pipe(compass({
-      config_file: './config.rb',
-      css: '.css-cache',
-      sass: 'stylesheets/sass'
-    }))
-    .pipe(duration('sass files compiled'));
-
-  return watch({ glob: '.css-cache/**/*.css' }, function(files) {
-    return files
-      .pipe(plumber())
-      .pipe(cmq())
-      .pipe(csso())
-      .pipe(gulp.dest('stylesheets/css'))
-  });
-});
-
+// -------------------------------------------------------------------------------------------------------------------------
+// Style the Styles --------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
 gulp.task('styles', function() {
-  return runSequence('styles:sass', 'styles:opt');
+    // Minify and copy all Styles (except vendor scripts)
+    gulp.src('src/scss/**/*.scss')
+        // Styles for Dev. Env.
+        .pipe(plumber())
+        .pipe(sass({sourcemap: true, sourcemapPath: 'scss'}))
+        .pipe(autoprefixer('last 2 version', '> 5%', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'ios 7', 'android 4'))
+        .pipe(gulp.dest('dist/'))
+        // Styles for Production Env.
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/'))
+        .pipe(connect.reload());
 });
 
-// gulp watch || gulp watch --theme [theme-name]
-// run express server with livereload and compass
-gulp.task('watch', ['serve', 'styles:sass'], function() {
-  gulp.watch(sources.sass, ['styles:sass']);
-  gulp.watch(sources.watch, refresh);
+// -------------------------------------------------------------------------------------------------------------------------
+// Script the Scripts ------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('scripts', function() {
+    gulp.src('src/scripts/*.js')
+        // Scripts for Dev Env.
+        .pipe(plumber())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(browserify({
+            insertGlobals : true,
+            debug : !gulp.env.production
+        }))
+        .pipe(gulp.dest('dist/scripts'))
+        // Scripts for Production Env.
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(connect.reload());
 });
 
-// gulp || gulp --theme [theme-name]
-// build SCSS
-gulp.task('default', ['styles'], function() {
-  // place code for your default task here
+// -------------------------------------------------------------------------------------------------------------------------
+// Compress images ---------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('images', function() {
+    gulp.src('src/images/**/*')
+        .pipe(plumber())
+        .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(connect.reload());
+});
 
+// -------------------------------------------------------------------------------------------------------------------------
+// Process HTML for PRODUCTION  --------------------------------------------------------------------------------------------
+// Replace the dev styles and scripts with minified and optimized versions -------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('html-production', function() {
+    gulp.src('src/markup/**/*.html')
+        .pipe(htmlreplace({
+            'css': 'style.min.css',
+            'js': 'main.min.js'
+        }))
+        .pipe(minifyHTML({comments:true, spare:true}))
+        .pipe(gulp.dest('dist/'));
+});
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Process HTML for DEVELOPMENT  -------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('html-dev', function() {
+    gulp.src('src/markup/**/*.html')
+        .pipe(gulp.dest('dist/'))
+        .pipe(connect.reload());
+});
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Start local server for Production ---------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('connect-dist', function() {
+    connect.server({
+        root: 'dist',
+        port: 8001
+    });
+});
+
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Start local server for DEVELOPMENT --------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('connect-dev', function() {
+    connect.server({
+        root: 'dist',
+        port: 8002,
+        livereload: true
+    });
+});
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Build for production ----------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('build-production', function() {
+    gulp.start('styles', 'scripts', 'html-production', 'images', 'connect-dist'); // Run $ gulp clean-dist when ready for production
+});
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Watch Task --------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('watch', function() {
+    gulp.watch('src/scss/**/*.scss', ['styles']);
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+    gulp.watch('src/markup/**/*.html', ['html-dev']);
+    gulp.watch('src/images/**/*', ['images']);
+});
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Start Dev enviroment ----------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('start-dev', function() {
+    gulp.start('styles', 'scripts', 'html-dev', 'images', 'connect-dev', 'watch');
+});
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Distribution Cleanup ----------------------------------------------------------------------------------------------------
+// Before a deploy it's safe to clean the distirbution ---------------------------------------------------------------------
+// Just to make sure nothing unwanted gets live ----------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('clean', function(cb) {
+    del(['dist/*.+(css|map|html)', 'dist/images', 'dist/scripts'], cb)
+});
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Clean distribution's leftovers ------------------------------------------------------------------------------------------
+// Remove the css.map's, unminified styles and scripts ---------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------
+gulp.task('clean-dist', function() {
+    del(["dist/*.map", 'dist/style.css', 'dist/scripts/main.js'])
 });
---
+++ src/scripts/custom.js
@@ -0,0 +1,18 @@
+// $('.nav-toggle').click(function() {
+// 	$(this).toggleClass('active');
+// 	$(this).siblings('ul').toggleClass('active');
+// });
+
+// var listFullHeight = function(element) {
+// 	var elem = $(element),
+// 		wh = $(window).height(),
+// 		headHeight = $('header').height(),
+// 		finalHeight = wh - headHeight;
+
+// 	elem.children().height(finalHeight);
+// 	// elem.children('li:nth-child(2), li:nth-child(3), li:nth-child(n+8)').height(finalHeight / 2);
+// 	console.log(wh, headHeight, finalHeight);
+
+// };
+
+// listFullHeight('.article-list');
